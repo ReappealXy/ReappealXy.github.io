797753fa35c24a3d0623003ed4945f8bd3985428
ReappealXy <reappealxy@gmail.com>
2025-03-15 23:02:13 +0800

    Site updated: 2025-03-15 23:02:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/11/hello-world/","2025/03/14/snake-game-md/"];function toRandomPost(){
+var posts=["2025/03/11/hello-world/","2025/03/15/Resource-sharing/","2025/03/14/snake-game/","2025/03/15/Request-forwarding-and-response-redirection/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
