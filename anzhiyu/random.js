deb968dd0c30eb842b4b5d914959f4401c9b5656
ReappealXy <reappealxy@gmail.com>
2025-03-24 09:31:47 +0800

    Site updated: 2025-03-24 09:31:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/19/ORM思想/","2025/03/23/1431-拥有最多糖果的孩子/","2025/03/11/hello-world/","2025/03/16/Relative-vs-absolute-paths/","2025/03/14/snake-game/","2025/03/15/Request-forwarding-and-response-redirection/","2025/03/23/什么东西/","2025/03/18/对于JDBC核心API的理解/"];function toRandomPost(){
+var posts=["2025/03/23/1431-拥有最多糖果的孩子/","2025/03/19/ORM思想/","2025/03/16/Relative-vs-absolute-paths/","2025/03/11/hello-world/","2025/03/15/Request-forwarding-and-response-redirection/","2025/03/18/对于JDBC核心API的理解/","2025/03/14/snake-game/","2025/03/23/什么东西/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
